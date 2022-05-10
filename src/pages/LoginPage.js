5ce935fa552fa55f3af79bed87f875690b4b0495
Noe Zavaleta C <nzavaletac@gmail.com>
2022-05-10 15:47:17 -0500

    Login connected to the backend successfully.

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import { Login } from "../components/Login/Login";
+import { URL_BACKEND } from "../environments/nvironments";
 
 export const LoginPage = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function login(e) {
+    e.preventDefault();
+    const {
+      data: { token },
+    } = await axios({
+      method: "POST",
+      baseURL: URL_BACKEND,
+      url: "/user/login",
+      data: {
+        email,
+        password,
+      },
+    });
+    localStorage.setItem("token", token);
+    navigate("/register");
+  }
+
   return (
     <>
-      <Login />
+      <Login login={login} setEmail={setEmail} setPassword={setPassword} />
     </>
   );
 };
