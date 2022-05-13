f795d990d025993c991ab6edb19eb24963e455be
Noe Zavaleta C <nzavaletac@gmail.com>
2022-05-12 23:29:57 -0500

    banner home page finished

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+  BannerImg,
+  Column,
+  Container,
+  Paragraph,
+  RegisterBtn,
+  Section,
+  Span,
+  SubTitle,
+  Title,
+} from "./BannerElements";
+import banner from "../../assets/images/banner1.svg";
+
+export const Banner = () => {
+  return (
+    <Container>
+      <Column>
+        <Section>
+          <SubTitle>EXCHANGE YOUR PRODUCTS</SubTitle>
+          <Title>
+            FAST AND <Span>EASY</Span>{" "}
+          </Title>
+          <Paragraph>EXCHANGE PRODUCTS IN YOUR CITY</Paragraph>
+          <RegisterBtn href="/register">Register now</RegisterBtn>
+        </Section>
+      </Column>
+      <Column>
+        <Section>
+          <BannerImg src={banner} />
+        </Section>
+      </Column>
+    </Container>
+  );
+};
