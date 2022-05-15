8fad2a3d396fcbb02e60126ac7aee916b4ebb01f
Noe Zavaleta C <nzavaletac@gmail.com>
2022-05-15 17:38:31 -0500

    component search design is finished

---
+++ src/components/Search/SearchElements.js
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-top: 30px;
+  margin-bottom: 15px;
+  width: 100%;
+`;
+
+export const Group = styled.div`
+  display: flex;
+  line-height: 28px;
+  align-items: center;
+  position: relative;
+  width: 30%;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  height: 40px;
+  line-height: 28px;
+  padding: 0 1rem;
+  padding-left: 2.5rem;
+  border: 2px solid transparent;
+  border-radius: 50px;
+  outline: none;
+  background-color: #f3f3f4;
+  color: #0d0c22;
+  transition: none.3s ease;
+  &::placeholder {
+    color: #9e9ea7;
+  }
+  &:focus,
+  &:hover {
+    outline: none;
+    border-color: gray;
+    background-color: #fff;
+    box-shadow: 0 0 0 4px rgb(234 76 137 / 10%);
+  }
+`;
+
+export const Svg = styled.svg`
+  position: absolute;
+  left: 1rem;
+  fill: #9e9ea7;
+  width: 1rem;
+  height: 1rem;
+`;
