a153a4c88bf98bed8dc0e78b82d5947c3f2e2201
Noe Zavaleta C <nzavaletac@gmail.com>
2022-05-15 18:18:20 -0500

    Aside of categories is finished

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,25 +1,13 @@
 import React from "react";
 import { Card } from "../Card/Card";
+import { Categories } from "../Categories/Categories";
 import { Aside, Body, Container } from "./MainElements";
 
 export const Main = ({ products }) => {
   return (
     <Container>
       <Aside>
-        <ul>
-          <li>All</li>
-          <li>Accesorios</li>
-          <li>Books</li>
-          <li>Beauty</li>
-          <li>Clothing</li>
-          <li>Footwear</li>
-          <li>Home</li>
-          <li>Technology</li>
-          <li>Toys</li>
-          <li>Sliorts</li>
-          <li>Video game</li>
-          <li>Others</li>
-        </ul>
+        <Categories />
       </Aside>
       <Body>
         <Card products={products} />
